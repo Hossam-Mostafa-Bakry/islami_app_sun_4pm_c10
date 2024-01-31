c54a69a024242ee582ab78098d7c7980994819d6
hossam.mbakry <hossam.mbakry@etisalat.com>
2024-01-31 16:51:07 +0200

    * initialization for islami app * start on quran feature

---
+++ lib/moduls/radio/page/radio_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class RadioView extends StatelessWidget {
+  const RadioView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Radio View",
+        style: TextStyle(
+            fontFamily: "El Messiri",
+            fontSize: 40,
+            fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+}
